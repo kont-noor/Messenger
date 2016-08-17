98d74b8fb21463818f54535df17041d8e59aff4b
Nickolay Kondratenko <devmarkup@gmail.com>
2016-08-17 17:32:58 +0300

    main app layout

--- index.ios.js
+++ index.ios.js
@@ -16,16 +16,11 @@ class Messenger extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.heading}>Messenger</Text>
+        <View style={styles.main}>
+          <Text>Content</Text>
+        </View>
+        <Text style={styles.copyright}>© Cats-3x, 2016</Text>
       </View>
     );
   }
@@ -34,19 +29,26 @@ class Messenger extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    paddingTop: 20
   },
-  welcome: {
+  heading: {
     fontSize: 20,
+    fontWeight: 'bold',
     textAlign: 'center',
-    margin: 10,
+    backgroundColor: '#ccc',
+    padding: 20
   },
-  instructions: {
+  main: {
+    flex: 1,
+    padding: 5
+  },
+  copyright: {
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: '#333',
+    color: '#ccc',
+    fontSize: 8,
+    padding: 5
   },
 });
 
