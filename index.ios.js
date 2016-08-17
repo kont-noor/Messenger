65214e9d5eb2672b73ea9ae698d31c474cfbd4f5
Nickolay Kondratenko <devmarkup@gmail.com>
2016-08-17 18:34:52 +0300

    push messages to the list

--- index.ios.js
+++ index.ios.js
@@ -7,8 +7,11 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  ScrollView,
   StyleSheet,
   Text,
+  TextInput,
+  TouchableHighlight,
   View
 } from 'react-native';
 
@@ -18,9 +21,45 @@ class Messenger extends Component {
       <View style={styles.container}>
         <Text style={styles.heading}>Messenger</Text>
         <View style={styles.main}>
-          <Text>Content</Text>
+          <Message />
         </View>
         <Text style={styles.copyright}>© Cats-3x, 2016</Text>
+      </View>
+    );
+  }
+}
+
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: '', messages: []};
+  }
+
+  send(text) {
+    if (text.length == 0)
+      return;
+
+    console.log(text);
+    this.setState({messages: [text, ...this.state.messages], text: ''});
+  }
+
+  render() {
+    return (
+      <View style={{padding: 10}}>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Message"
+          onChangeText={text => this.setState({text})}
+          value={this.state.text}
+        />
+
+        <View style={styles.send}>
+          <TouchableHighlight onPress={this.send.bind(this, this.state.text)}>
+            <Text style={styles.sendText}>Send</Text>
+          </TouchableHighlight>
+        </View>
+
+        {this.state.messages.map((m, i) => <Text key={i}>{m}</Text>)}
       </View>
     );
   }
@@ -42,6 +81,14 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
     padding: 5
+  },
+  send: {
+    borderWidth: 1,
+    width: 100,
+    height: 20
+  },
+  sendText: {
+    textAlign: 'center',
   },
   copyright: {
     textAlign: 'center',
